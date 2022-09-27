a1b6600d0ebd905095ee1d8dbf05dd3f48a5177f
pok2d <113487242+pok2d@users.noreply.github.com>
2022-09-27 19:55:50 -0400

    Forgot to tab LOL

--- main.py
+++ main.py
@@ -93,59 +93,59 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Event for what happens with mouse down clicked.
-    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 
-        mouseposition = pygame.mouse.get_pos()
-        posx = mouseposition[0]
-        posy = mouseposition[1]
-        # There is an alternation system here; if the game is in progress, the game only checks for clicks at spots,
-        # not at where the restart button would be. However, if the game ended (someone won) it swaps, and it will then
-        # only check for clicks at the now loaded restart button and not at any of the spots.
-        if restartbuttonactive == False:
-            # Here checks is the click is within one of the 9 areas for where an x or o can be.
-            if 316 > posx > 196 and 216 > posy > 96:  # Top Left
-                # print('Top Left')
-                addactivespot(1)
+            mouseposition = pygame.mouse.get_pos()
+            posx = mouseposition[0]
+            posy = mouseposition[1]
+            # There is an alternation system here; if the game is in progress, the game only checks for clicks at
+            # spots, not at where the restart button would be. However, if the game ended (someone won) it swaps,
+            # and it will then only check for clicks at the now loaded restart button and not at any of the spots.
+            if restartbuttonactive == False:
+                # Here checks is the click is within one of the 9 areas for where an x or o can be.
+                if 316 > posx > 196 and 216 > posy > 96:  # Top Left
+                    # print('Top Left')
+                    addactivespot(1)
 
-            if 316 > posx > 196 and 360 > posy > 240:  # Left Edge
-                # print('Left Edge')
-                addactivespot(2)
+                if 316 > posx > 196 and 360 > posy > 240:  # Left Edge
+                    # print('Left Edge')
+                    addactivespot(2)
 
-            if 316 > posx > 196 and 504 > posy > 384:  # Bottom Left
-                # print('Bottom Left')
-                addactivespot(3)
+                if 316 > posx > 196 and 504 > posy > 384:  # Bottom Left
+                    # print('Bottom Left')
+                    addactivespot(3)
 
-            if 460 > posx > 340 and 216 > posy > 96:  # Top Edge
-                # print('Top Edge')
-                addactivespot(4)
+                if 460 > posx > 340 and 216 > posy > 96:  # Top Edge
+                    # print('Top Edge')
+                    addactivespot(4)
 
-            if 460 > posx > 340 and 360 > posy > 240:  # Center
-                # print('Center')
-                addactivespot(5)
+                if 460 > posx > 340 and 360 > posy > 240:  # Center
+                    # print('Center')
+                    addactivespot(5)
 
-            if 460 > posx > 340 and 504 > posy > 384:  # Bottom Edge
-                # print('Bottom Edge')
-                addactivespot(6)
+                if 460 > posx > 340 and 504 > posy > 384:  # Bottom Edge
+                    # print('Bottom Edge')
+                    addactivespot(6)
 
-            if 604 > posx > 484 and 216 > posy > 96:  # Top Right
-                # print('Top Right')
-                addactivespot(7)
+                if 604 > posx > 484 and 216 > posy > 96:  # Top Right
+                    # print('Top Right')
+                    addactivespot(7)
 
-            if 604 > posx > 484 and 360 > posy > 240:  # Right Edge
-                # print('Right Edge')
-                addactivespot(8)
+                if 604 > posx > 484 and 360 > posy > 240:  # Right Edge
+                    # print('Right Edge')
+                    addactivespot(8)
 
-            if 604 > posx > 484 and 504 > posy > 384:  # Bottom Right
-                # print('Bottom Right')
-                addactivespot(9)
-        else:
-            if 178 > posx > 50 and 180 > posy > 100:  # Restart button clicked
-                # Sets the restart button value to false to basically reset the game.
-                # Also clears all the lists, since empty lists are neccesary to play again.
-                restartbuttonactive = False
-                activespots.clear()
-                activespotsx.clear()
-                activespotso.clear()
+                if 604 > posx > 484 and 504 > posy > 384:  # Bottom Right
+                    # print('Bottom Right')
+                    addactivespot(9)
+            else:
+                if 178 > posx > 50 and 180 > posy > 100:  # Restart button clicked
+                    # Sets the restart button value to false to basically reset the game.
+                    # Also clears all the lists, since empty lists are neccesary to play again.
+                    restartbuttonactive = False
+                    activespots.clear()
+                    activespotsx.clear()
+                    activespotso.clear()
     # Loads the background.
     screen.fill((255, 255, 255))
     screen.blit(background, (196, 96))
